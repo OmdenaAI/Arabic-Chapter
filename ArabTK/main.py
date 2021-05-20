64960c699347b53b89e93126b2d756bb7353f78d
suriya-it19 <54205777+suriya-it19@users.noreply.github.com>
2021-05-20 14:51:56 +0530

    Updated Stop word removing function

--- ArabTK/main.py
+++ ArabTK/main.py
@@ -1,15 +1,17 @@
 from utils import tokenize
+from utils import stop_words
 
 
 
 
 if __name__ == '__main__':
     text = " عقلاً وضميرًا وعليهم أن يعامل بعضهم بعضًا بروح الإخاء."
-    # text = text.decode('utf-8')
+    #text = "Hello all, It's Suriya. How are you all?"
+    text.decode(encoding='utf-8')
 
     #Class Initialization
     # available methods : reg_exp, keras, word_punkt, tree_bank, default : word_tokenize 
-    tokenizer = tokenize.ArabicTokenizer(word_method="reg_exp")
+    tokenizer = tokenize.ArabicTokenizer(word_method="word_punkt")
 
     #Sentence tokenization, Not necessary!
     text = tokenizer.sentence_tokenize(text)
@@ -17,7 +19,11 @@ if __name__ == '__main__':
     print(text)
 
     #Word tokenizer to split sentences or paragraphs to tokens
-    words = []
-    for i in text:
-        words.extend(tokenizer.word_tokenize(i))
-    print(words)
+    text = tokenizer.word_tokenize(text)
+    print(len(text))
+    print(text)
+
+    #to remove basic un wanted words, feel free to add stop words corpus in the data\Stop_words folder
+    text = stop_words.remove_stop_words(text)
+    print(len(text))
+    print(text)
