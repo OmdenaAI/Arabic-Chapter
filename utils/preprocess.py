070422e4ee9ce943a64fbf73ca69b49fc212fd5f
suriya-it19 <54205777+suriya-it19@users.noreply.github.com>
2021-06-03 21:07:53 +0530

    tuned tokenization func

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -10,17 +10,15 @@ def tokenizer(
     maxlen = 0
     for i in range(len(texts)):
         maxlen = len(texts[i]) if len(texts[i]) > maxlen else maxlen
-        for x in texts[i].lower(): 
-            if x in punctuations: 
-                texts[i] = texts[i].replace(x, "")
+        for x in texts[i]: 
+            if x not in punctuations: 
+                texts[i] = texts[i].replace(x, " ")
 
         texts[i] = re.sub(r'\w*\d\w*', '', texts[i])
 
         texts[i] = re.sub(r'[0-9]+', '', texts[i])
 
         texts[i] = re.sub(r'\s+', ' ', texts[i]).strip()
-
-        texts[i] = texts[i].lower()
 
         texts[i] = texts[i].split(' ')
 
--- word_embedding.py
+++ word_embedding.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+from sklearn import preprocessing, model_selection
 from scipy import sparse
 
 import tensorflow as tf
@@ -14,7 +15,7 @@ from utils import helper
 
 
 class WordEmbedding:
-    def __init__(self, tokenizer, vocab_size=30, maxlen=10, embedding_vector=5, method="keras"):
+    def __init__(self, tokenizer, vocab_size=10000, maxlen=256, embedding_vector=5, method="keras"):
         self.method = method
         self.tokenizer = tokenizer
         self.vocab_size = vocab_size
@@ -25,7 +26,7 @@ class WordEmbedding:
         tokens, maxlen = self.tokenizer(text, stop_words)
         return tokens
 
-    def encode(self, text):
+    def encode(self, text, label):
         vector, temp, all_ = [], [], []
         for d in text:
             for i in d:
@@ -36,19 +37,22 @@ class WordEmbedding:
         for x in text:
             all_.extend(x)
         word_dict = helper.word_dictionary(all_)
-        return vector, list(word_dict.keys()), word_dict
+        onehot_encoder = preprocessing.OneHotEncoder(sparse=False)
+        label = np.array(label).reshape(len(label), 1)
+        label = onehot_encoder.fit_transform(label)
+        return vector, label, list(word_dict.keys()), word_dict
 
-    def train_keras(self, text, label, epochs=5):
-        #text = self.encode(text)
+    def train_keras(self, text, label, epochs=5, validation_split=0.2):
+        trainX, validX, trainY, validY = model_selection.train_test_split(text, label, test_size=validation_split, random_state=42, shuffle=True)
 
-        model = helper.get_model(text, label, self.vocab_size, self.embedding_vector, self.maxlen)
-        model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+        model = helper.get_model(trainX, trainY, self.vocab_size, self.embedding_vector, self.maxlen)
+        model.compile(optimizer="adam", loss=tf.keras.losses.CategoricalCrossentropy(), metrics=["accuracy"])
         print(model.summary())
 
-        es = EarlyStopping(monitor='loss', mode='min', verbose=1,patience=3)  
-        mc = ModelCheckpoint('models/word_embeddings.h5', monitor='loss', mode='min', save_best_only=True,verbose=1)
+        es = EarlyStopping(monitor='val_loss', mode='min', verbose=1,patience=3)  
+        mc = ModelCheckpoint('models/word_embeddings_NN.h5', monitor='val_loss', mode='min', save_best_only=True,verbose=1)
 
-        model.fit(text, label, epochs=epochs, callbacks=[es, mc], verbose=1)
+        model.fit(trainX, trainY, validation_data=(validX, validY), epochs=epochs, callbacks=[es, mc], verbose=1)
 
         return model
 
@@ -59,15 +63,17 @@ class WordEmbedding:
         #print(X, Y)
         return X, Y, list(word_dict.keys()), word_dict
 
-    def train_w2v(self, words, label, epochs=5):
-        model = helper.get_model(words, label, self.vocab_size, self.embedding_vector, self.maxlen, method=self.method)
+    def train_w2v(self, words, label, epochs=5, validation_split=0.2):
+        trainX, validX, trainY, validY = model_selection.train_test_split(words, label, test_size=validation_split, random_state=42, shuffle=True)
+
+        model = helper.get_model(trainX, trainY, self.vocab_size, self.embedding_vector, self.maxlen, method=self.method)
         model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
         print(model.summary())
 
-        es = EarlyStopping(monitor='loss', mode='min', verbose=1,patience=3)  
-        mc = ModelCheckpoint('models/word_embeddings.h5', monitor='loss', mode='min', save_best_only=True,verbose=1)
+        es = EarlyStopping(monitor='val_loss', mode='min', verbose=1,patience=3)  
+        mc = ModelCheckpoint('models/word_embeddings_w2v.h5', monitor='val_loss', mode='min', save_best_only=True,verbose=1)
 
-        model.fit(words.toarray(), label.toarray(), epochs=epochs, callbacks=[es, mc], verbose=1)
+        model.fit(trainX.toarray(), trainY.toarray(), validation_data=(validX.toarray(), validY.toarray()), epochs=epochs, callbacks=[es, mc], verbose=1)
 
         return model
 
