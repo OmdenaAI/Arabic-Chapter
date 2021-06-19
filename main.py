0197728f9b3e92e112bda23aaa3fdb1f0a9daff4
suriya-it19 <54205777+suriya-it19@users.noreply.github.com>
2021-06-19 10:07:28 +0530

    main update

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-
+import tensorflow as tf
 from models.Word2Vec import Word2Vec
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
@@ -9,6 +9,7 @@ import numpy as np
 import pandas as pd
 
 from utils import helper, preprocess
+from utils.config import config
 
 """
 Yet to do:
@@ -18,22 +19,17 @@ Yet to do:
 
 if __name__ == '__main__':
 
-    data = pd.read_excel("data/AJGT.xlsx")
-    # text = list(data.Feed)
-    # label = list(data.Sentiment)
+    data = pd.read_csv("data/IMDB Dataset.csv")
 
+    # text = list(data.review)
+    # label = list(data.sentiment)
 
     # #Keras Neural net features
-    # embeddings = Word2Vec(preprocess.tokenizer, vocab_size=10000, maxlen=80, embedding_vector=10, method="keras")
-    # text = embeddings.tokenize(text, stop_words=[' ', '0', '1', '2', '3', '4', '5', '6',
-    #                                             '7', '8', '9', '?', 
-    #                                             '؟', 'ء', 'ؤ', 'ئ', 'ا', 'ب', 'ت', 'ث',
-    #                                             'ج', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'س', 'ش', 'ص', 'ض', 'ط', 'ظ',
-    #                                             'ع', 'غ', 'ف', 'ق', 'ك', 'ل', 'م', 'ن', 'ه', 'و', 'ي', '٠', '١',
-    #                                             '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'])
+    # embeddings = Word2Vec(preprocess.tokenizer, vocab_size=config['vocab_size'], maxlen=config['maxlen'], embedding_vector=config['embedding_vector'], method=config['method'])
+    # text = embeddings.tokenize(text, stop_words=config['stop_words'], punctuations=config['punctuations'])
     # text, label, unique_words, word_dict = embeddings.encode(text, label)
     # text, label = np.array(text), np.array(label)
-    # model = embeddings.train_keras(text, label, epochs=50, validation_split=0.2)
+    # model = embeddings.train_keras(text, label, epochs=config['epochs'], validation_split=0.2)
 
     # word_embeddings = model.get_layer("embedding").get_weights()[0]
 
@@ -42,21 +38,25 @@ if __name__ == '__main__':
     # helper.save_embeddings(embeddings, "embeddings_NN")
 
 
-    text = list(data.Feed)
-    label = list(data.Sentiment)
+    text = list(data.review)
+    label = list(data.sentiment)
+    # print(text[0])
+    # print("\n")
 
     # Word2vec
-    embeddings = Word2Vec(preprocess.tokenizer, vocab_size=10000, maxlen=80, embedding_vector=5, method="word2vec")
-    text = embeddings.tokenize(text, stop_words=[' ', '0', '1', '2', '3', '4', '5', '6',
-                                                '7', '8', '9', '?', 
-                                                '؟', 'ء', 'ؤ', 'ئ', 'ا', 'ب', 'ت', 'ث',
-                                                'ج', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'س', 'ش', 'ص', 'ض', 'ط', 'ظ',
-                                                'ع', 'غ', 'ف', 'ق', 'ك', 'ل', 'م', 'ن', 'ه', 'و', 'ي', '٠', '١',
-                                                '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'])
-    words, label, unique_words, word_dict = embeddings.encode_w2v(text)
-    model = embeddings.train_w2v(words, label, epochs=50, validation_split=0.2)
+    embeddings = Word2Vec(preprocess.tokenizer, vocab_size=config['vocab_size'], maxlen=config['maxlen'], embedding_vector=config['embedding_vector'], method=config['method'])
+    text = embeddings.tokenize(text, stop_words=config['stop_words'], punctuations=config['punctuations'])
+    # print(text[0])
+    # print("\n")
+    words, label, unique_words, word_dict = embeddings.encode_w2v(text, window_size=config['window_size'])
+    # # print(words[0:5], label[0:5])
+    model = embeddings.train_w2v(words, label, epochs=config['epochs'], validation_split=0.1)
+    model = tf.keras.models.load_model("models/word_embeddings_w2v.h5")
+    print(model.predict([[word_dict['king'], word_dict['has']]]))
+    print(word_dict["won"])
 
     word_embeddings = model.get_weights()[0]
+    print(np.sum(word_embeddings, axis=0))
 
     embeddings = helper.get_embeddings(unique_words, word_dict, word_embeddings)
     helper.plot(word_dict, embeddings, "embeddings_w2v")
--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,30 +1,65 @@
 import re
+from tqdm import tqdm
+
+# def tokenizer(
+#     texts:list,
+#     punctuations = """'!"#$%&'()*+,«».؛،/:؟?@[\]^_`{|}~""",
+#     stop_words=['and', 'a', 'is', 'the', 'in', 'be', 'will']
+#     )->list:
+
+#     list_ = []
+#     maxlen = 0
+#     for i in range(len(texts)):
+#         maxlen = len(texts[i]) if len(texts[i]) > maxlen else maxlen
+#         for x in texts[i]: 
+#             if x not in punctuations: 
+#                 texts[i] = texts[i].replace(x, " ")
+#             # if x == 'و':
+#             #     texts[i] = texts[i].replace(x, " و ")
+
+#         texts[i] = re.sub(r'\w*\d\w*', '', texts[i])
+
+#         texts[i] = re.sub(r'[0-9]+', '', texts[i])
+
+#         texts[i] = re.sub(r'\s+', ' ', texts[i]).strip()
+
+#         texts[i] = texts[i].split(' ')
+
+#         texts[i] = [x for x in texts[i] if x!='']
+
+#         texts[i] = [x for x in texts[i] if x not in stop_words]
+#         list_.append(texts[i])
+
+#     return list_, maxlen
 
 def tokenizer(
     texts:list,
-    punctuations = """'!"#$%&'()*+,«».؛،/:؟?@[\]^_`{|}~""",
-    stop_words=['and', 'a', 'is', 'the', 'in', 'be', 'will']
+    punctuations=[],
+    stop_words=[],
     )->list:
 
     list_ = []
     maxlen = 0
-    for i in range(len(texts)):
-        maxlen = len(texts[i]) if len(texts[i]) > maxlen else maxlen
-        for x in texts[i]: 
-            if x not in punctuations: 
-                texts[i] = texts[i].replace(x, " ")
+    vocab = 0
+    for i in tqdm(range(len(texts)), total=len(texts)):
+        texts[i] = texts[i].lower()
+        for x in texts[i]:
+           if x in punctuations:
+               texts[i] = texts[i].replace(x, " ")
 
+        texts[i] = re.sub(r'<.*?>', '', texts[i])
         texts[i] = re.sub(r'\w*\d\w*', '', texts[i])
-
         texts[i] = re.sub(r'[0-9]+', '', texts[i])
-
         texts[i] = re.sub(r'\s+', ' ', texts[i]).strip()
 
         texts[i] = texts[i].split(' ')
-
         texts[i] = [x for x in texts[i] if x!='']
-
+        texts[i] = [x for x in texts[i] if x not in punctuations]
         texts[i] = [x for x in texts[i] if x not in stop_words]
         list_.append(texts[i])
 
-    return list_, maxlen
+        maxlen = len(texts[i]) if len(texts[i]) > maxlen else maxlen
+        for i in texts[i]:
+            vocab += 1
+                
+    return list_, maxlen, vocab
