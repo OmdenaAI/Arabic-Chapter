2c5b1f8a6803e5a344992bcba9102d6a0ad14cd4
suriya-it19 <54205777+suriya-it19@users.noreply.github.com>
2021-06-19 21:25:19 +0530

    path changes

--- NER/input/data_generation.py
+++ NER/input/data_generation.py
@@ -3,18 +3,18 @@ import json
 from tqdm import tqdm
 import pandas as pd
 
-input_ = ["apprentissage_ne/", "test_ne/"]
+input_ = ["NER/input/apprentissage_ne/", "NER/input/test_ne/"]
 
 all_text = dict()
 
 for folder in input_:
     for file in tqdm(glob.glob(folder + "*.txt"), total=len(glob.glob(folder + "*.txt"))):
-        f = open(file, "r")
+        f = open(file, "r", encoding='utf-8')
         for i in f:
             label, text = i.strip().split(':')
             all_text[text.strip()] = label
 #df = pd.DataFrame(all_text, columns=["text", "label"], index=[0])
 #df.to_csv("all.csv")
 
-with open('data.json', 'w') as fp:
+with open('NER/input/data.json', 'w') as fp:
     json.dump(all_text, fp)
