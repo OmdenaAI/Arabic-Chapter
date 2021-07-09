f15282f00988caf0259c4a8da7290b03250c259e
Eman Elrefai <30290345+EmanElrefai@users.noreply.github.com>
2021-07-09 22:06:55 +0200

    Delete Models directory

--- Models/morphological-modelling/test.py
+++
@@ -1,8 +0,0 @@
-from morphological_analysis.morphological_modelling import Morphology
-
-morph = Morphology()
-print(morph.word_morphology('اليوم', 'noun'))
-print(morph.word_morphology('شربكم', 'noun'))
-print(morph.word_morphology('المظلومون', 'noun'))
-print(morph.word_morphology('مدرستهم', 'noun'))
-print(morph.word_morphology('فاسقيناكموه', 'verb'))
