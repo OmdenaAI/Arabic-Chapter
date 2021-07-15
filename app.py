06454fca1475f80a3b6adcba4ca7a4455617e6f1
Mohamed Mohey <mmi9433@gmail.com>
2021-07-15 08:19:22 +0200

    minor changes

--- app.py
+++ app.py
@@ -2,23 +2,17 @@ from flask import Flask, jsonify, request
 from word_embedding.embedding import WordEmbedding
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
 
 
 @app.route("/", methods=['GET'])
 def root():
     return jsonify({
-        'embeddings' : "use /aravec/embedding?text=" " to get embeddings for each token",
-        'similar words' : "to get similar words use /aravec/similar/"
+        'similar words' : "to get similar words use /aravec/similar/ (or word2vec)",
+        'analogy': "use /aravec/analogy?text=" " to get analogies (or word2vec)"
+
     })
 
-
-@app.route("/aravec/embedding", methods=['GET', 'POST'])
-def predict_aravec():
-    text = request.args.get('text')
-    em = WordEmbedding(text=text, method="aravec")
-    text = em.get_preprocessed_aravec()
-    model = em.get_model()
-    return em.get_embeddings(text, model)
 
 @app.route("/aravec/analogy", methods=['GET', 'POST'])
 def predict_aravec_analogy():
@@ -36,14 +30,6 @@ def predict_aravec_similar():
     model = em.get_model()
     return em.get_similar(text, model)
 
-@app.route("/word2vec/embedding", methods=['GET', 'POST'])
-def predict_word2vec():
-    positive = request.args.get('positive')
-    negative = request.args.get('negative')
-    em = WordEmbedding(text=positive, method="word2vec")
-    text = em.get_preprocessed_word2vec()
-    model = em.get_model()
-    return em.get_similar(text, model)
 
 @app.route("/word2vec/analogy", methods=['GET', 'POST'])
 def predict_word2vec_analogy():
